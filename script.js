7589c60b7b9402426aa9be412da6955e3f85cd77
RIANPURNAMA1 <122508008+RIANPURNAMA1@users.noreply.github.com>
2023-03-28 04:44:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ $('#hider').click(function(){
+    $('#parag').hide(1000);
+ });
+
+});
+$(document).ready(function(){
+    $('#show').click(function(){
+       $('#parag').show(1000);
+    });
+   
+   });
